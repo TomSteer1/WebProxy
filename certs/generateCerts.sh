745c105022c2a54774c66fedacce95485d4724ef
Tom <tom@tomsteer.me>
2024-05-02 01:32:08 +0100

    Removed test certs

--- certs/generateCerts.sh
+++ certs/generateCerts.sh
@@ -10,10 +10,10 @@ openssl req -x509 -new -nodes -key ca.key -sha256 -days 365 -out ca.crt -subj "/
 openssl genrsa -out wildcard.key 2048
 
 # Generate a certificate signing request (CSR) for the wildcard domain
-openssl req -new -key wildcard.key -out wildcard.csr -subj "/CN=*.com"
+openssl req -new -key wildcard.key -out wildcard.csr -subj "/CN=duckduckgo.com" 
 
 # Generate a certificate for the wildcard domain signed by the CA
-openssl x509 -req -in wildcard.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out wildcard.crt -days 365 -sha256
+openssl x509 -req -in wildcard.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out wildcard.crt -days 365 -sha256 -extfile <(printf "subjectAltName=DNS:*.*,DNS:duckduckgo.com,DNS:*.com")
 
 # Clean up the CSR and CA serial files
 rm wildcard.csr ca.srl
